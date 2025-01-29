858c549d486101119814fc510c68ccfa7a199db6
babajuneed <babajuneed@gmail.com>
2025-01-29 23:30:23 +0530

    added the new files main.tf and varaible.tf

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "filename" {
+    default = "/root/animals.txt"
+}
+variable "content" {
+ default = "I love animals"
+ }
+variable "prefix" {
+    default = "Pet"
+}
+variable "separator" {
+    default = "-"
+}
+variable "length" {
+    default = 2
+}
